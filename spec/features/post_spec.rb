0ced65634b3872906dc1f37c899773ccb046254a
Mckaid Gustafson <mckaidcgustafson@gmail.com>
2017-05-15 14:50:34 -0600

    ADded new post button to navbar

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -27,6 +27,8 @@ describe 'navigate' do
     end
   end
 
+
+
   describe 'creation' do
   	before do
   		visit new_post_path
@@ -53,6 +55,15 @@ describe 'navigate' do
     end
   end
 
+  describe 'new' do
+    it 'has a link from the homepage' do
+      visit root_path
+
+      click_link("Add New Entry")
+      expect(page.status_code).to eq(200)
+    end
+  end
+
   describe 'edit' do
     before do
       @post = FactoryGirl.create(:post)
@@ -64,15 +75,25 @@ describe 'navigate' do
       click_link("edit_#{@post.id}")
       expect(page.status_code).to eq(200)
     end
+  end
 
-    # it 'can be edited' do
-    #   visit edit_post_path(@post)
-    #
-    #   fill_in 'post[date]', with: Date.today
-    #   fill_in 'post[rationale]', with: "Edited content"
-    #   click_on "Save"
-    #
-    #   expect(page).to have_content("Edited content")
-    # end
+  describe 'delete' do
+    it 'can be deleted' do
+      @post = FactoryGirl.create(:post)
+      visit posts_path
+
+      click_link("delete_post_#{@post.id}_from_index")
+      expect(page.status_code).to eq(200)
+    end
+  end
+
+    describe 'delete' do
+    it 'can be deleted' do
+      @post = FactoryGirl.create(:post)
+      visit posts_path
+
+      click_link("delete_post_#{@post.id}_from_index")
+      expect(page.status_code).to eq(200)
+    end
   end
 end
