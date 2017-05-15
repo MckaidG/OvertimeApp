c147a80814adcfa9da8b25fda9ab7dd28fa2f38b
Mckaid Gustafson <mckaidcgustafson@gmail.com>
2017-05-15 14:33:11 -0600

    Implemented edit and update functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show]
+  before_action :set_post, only: [:show, :edit]
   def index
     @posts = Post.all
   end
@@ -20,8 +20,18 @@ class PostsController < ApplicationController
   end
 
   def show
-
   end
+
+  def edit
+  end
+
+  def update
+		if @post.update(post_params)
+			redirect_to @post, notice: 'Your post was created successfully'
+		else
+			render :edit
+		end
+	end
 
   private
     def post_params
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,19 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   describe "Creation" do
-    before do
-       user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow") #added a user
-       login_as(user, :scope => :user) #logged in as that user
-       @post = Post.create(date: Date.today, rationale: "Anything", user_id: user.id) #set user_id for the post
-     end
-    it 'can be created' do
-      expect(@post).to be_valid
-    end
+  	before do
+  		@post = FactoryGirl.create(:post)
+  	end
 
-    it 'cannont be created without a date and rationale' do
-      @post.date = nil
-      @post.rationale = nil
-      expect(@post).to_not be_valid
-    end
+  	it 'can be created' do
+  		expect(@post).to be_valid
+  	end
+
+  	it 'cannot be created without a date and rationale' do
+  		@post.date = nil
+  		@post.rationale = nil
+  		expect(@post).to_not be_valid
+  	end
   end
 end
