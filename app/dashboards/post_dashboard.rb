1623cb2cc88fb58aeda912d8ce1d15f37e4cc501
Mckaid Gustafson <mckaidcgustafson@gmail.com>
2017-05-16 17:06:33 -0600

    Implemented edit functionality for status

--- app/dashboards/post_dashboard.rb
+++ app/dashboards/post_dashboard.rb
@@ -8,12 +8,13 @@ class PostDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
-    user: Field::BelongsTo,
-    id: Field::Number,
-    date: Field::DateTime,
-    rationale: Field::Text,
-    created_at: Field::DateTime,
-    updated_at: Field::DateTime,
+    user: Field::BelongsTo.with_options(searchable: false),
+     id: Field::Number.with_options(searchable: false),
+     date: Field::DateTime.with_options(searchable: false),
+     rationale: Field::Text.with_options(searchable: true),
+     created_at: Field::DateTime.with_options(searchable: false),
+     updated_at: Field::DateTime.with_options(searchable: false),
+     status: Field::Text.with_options(searchable: true),
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -23,20 +24,21 @@ class PostDashboard < Administrate::BaseDashboard
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
     :user,
-    :id,
-    :date,
-    :rationale,
+     :status,
+     :date,
+     :rationale,
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
     :user,
-    :id,
-    :date,
-    :rationale,
-    :created_at,
-    :updated_at,
+     :status,
+     :id,
+     :date,
+     :rationale,
+     :created_at,
+     :updated_at,
   ].freeze
 
   # FORM_ATTRIBUTES
@@ -44,8 +46,8 @@ class PostDashboard < Administrate::BaseDashboard
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
     :user,
-    :date,
-    :rationale,
+   :date,
+   :rationale,
   ].freeze
 
   # Overwrite this method to customize how posts are displayed
---
+++ db/migrate/20170516173916_delete_status_from_posts.rb
@@ -0,0 +1,5 @@
+class DeleteStatusFromPosts < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :posts, :status, :integer
+  end
+end
