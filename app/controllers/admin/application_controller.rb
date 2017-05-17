1011d1c4cb8e3eade7f82e5913b2f6b500f82759
Mckaid Gustafson <mckaidcgustafson@gmail.com>
2017-05-17 16:25:43 -0600

    Implemented tests and page redirection for permission structure

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,3 +1,9 @@
+# All Administrate controllers inherit from this `Admin::ApplicationController`,
+# making it the ideal place to put authentication logic or other
+# before_filters.
+#
+# If you want to add pagination or other controller-level concerns,
+# you're free to overwrite the RESTful controller actions.
 module Admin
   def self.admin_types
     ['AdminUser']
@@ -5,13 +11,19 @@ module Admin
 
   class ApplicationController < Administrate::ApplicationController
     before_action :authenticate_user!
-    before_filter :authenticate_admin
+    before_action :authenticate_admin
 
     def authenticate_admin
-      unless current_user.try(:type) == 'AdminUser'
+      unless Admin.admin_types.include?(current_user.try(:type))
         flash[:alert] = "You are not authorized to access this page."
         redirect_to(root_path)
       end
     end
+
+    # Override this value to specify the number of elements to display at a time
+    # on index pages. Defaults to 20.
+    # def records_per_page
+    #   params[:per_page] || 20
+    # end
   end
 end
