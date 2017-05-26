869d0e0fd9418c65d478be24b573a7ce71a1f970
Mckaid Gustafson <mckaidcgustafson@gmail.com>
2017-05-26 15:22:25 -0600

    heroku 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-@employee = Employee.create(email: "test@test.com",
+@employee = Employee.create(email: "test1@test.com",
                     password: "asdfasdf",
                     password_confirmation: "asdfasdf",
                     first_name: "Jon",
@@ -7,7 +7,7 @@
 
 puts "1 employee created"
 
-AdminUser.create(email: "admin@test.com",
+AdminUser.create(email: "admin1@test.com",
 									password: "asdfasdf",
 									password_confirmation: "asdfasdf",
 									first_name: "Admin",
